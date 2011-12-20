5a133d6f79fd8c994123bf4bcd13d1156ccf5516
Yong Nie <nieyong_1947@126.com>
2011-12-21 06:48:35 +0800

    添加cling-support的jar库，完成目录搜索（content directory）的相关框架

--- src/org/teleal/cling/android/browser/BrowseDeviceActivity.java
+++ src/org/teleal/cling/android/browser/BrowseDeviceActivity.java
@@ -5,8 +5,18 @@ package org.teleal.cling.android.browser;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.teleal.cling.android.AndroidUpnpService;
 import org.teleal.cling.android.upnp.UpnpBrowserApp;
+import org.teleal.cling.model.action.ActionInvocation;
+import org.teleal.cling.model.message.UpnpResponse;
 import org.teleal.cling.model.meta.Device;
+import org.teleal.cling.model.meta.LocalService;
+import org.teleal.cling.model.meta.Service;
+import org.teleal.cling.model.types.ServiceId;
+import org.teleal.cling.support.contentdirectory.callback.Browse;
+import org.teleal.cling.support.model.BrowseFlag;
+import org.teleal.cling.support.model.DIDLContent;
+import org.teleal.cling.support.model.item.Item;
 
 
 
@@ -20,10 +30,16 @@ public class BrowseDeviceActivity extends ListActivity
 {   
 	private Device device;
 	
+	private AndroidUpnpService upnpService;
+	
+	private Service service;
+	
 	ArrayAdapter<String> adapter;
 	
     public void onCreate(Bundle savedInstanceState) { 
     	super.onCreate(savedInstanceState);
+    	
+    	
     	
     	
     	
@@ -32,24 +48,52 @@ public class BrowseDeviceActivity extends ListActivity
 	  
 	  device = ((UpnpBrowserApp) getApplication()).getDevice(position);
 	  
-        List<String> items = fillArray();
+        List<String> items = new ArrayList<String>();
         items.add((String)(device.getDetails().getFriendlyName()));
+        items.add((String)(device.getDetails().getSerialNumber()));
+        items.add((String)(device.getDetails().getModelDetails().getModelDescription()));
+        
+//        device.getServices()
+        
+        service = device.findService(new ServiceId("tt", null));
+        
+        upnpService.getControlPoint().execute(
+        		new Browse(service, "0", BrowseFlag.DIRECT_CHILDREN){
+
+					@Override
+					public void received(ActionInvocation arg0, DIDLContent arg1) {
+						// TODO Auto-generated method stub
+//				        assertEquals(arg1.getItems().size(), 2);
+				        Item item1 = arg1.getItems().get(0);
+						
+					}
+
+					@Override
+					public void updateStatus(Status arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void failure(ActionInvocation arg0,
+							UpnpResponse arg1, String arg2) {
+						// TODO Auto-generated method stub
+						
+					}
+        			
+        		}
+        		);
         
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items); 
                
         this.setListAdapter(adapter); 
     }
     
-    private List<String> fillArray() 
-    { 
-        List<String> items = new ArrayList<String>(); 
-        items.add("日曜日"); 
-        items.add("月曜日"); 
-        items.add("火曜日"); 
-        items.add("水曜日"); 
-        items.add("木曜日"); 
-        items.add("金曜日"); 
-        items.add("土曜日"); 
-        return items; 
-    } 
+
 }
+
+
+
+
+
+
